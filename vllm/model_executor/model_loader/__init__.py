7b682abc0fda0795c579864fa3ee3c8051a0f84a
nctu6.tw <nctu6.tw@gmail.com>
2024-10-29 15:56:17 +0800

    fix loop import

--- vllm/model_executor/model_loader/__init__.py
+++ vllm/model_executor/model_loader/__init__.py
@@ -2,8 +2,8 @@ from typing import Optional
 
 from torch import nn
 
-from vllm.config_indep import CacheConfig, DeviceConfig, LoadConfig, LoRAConfig
-from vllm.config import (ModelConfig, ParallelConfig, SchedulerConfig)
+from vllm.config_indep import CacheConfig, DeviceConfig, LoadConfig, LoRAConfig, ModelConfig
+from vllm.config import (ParallelConfig, SchedulerConfig)
 from vllm.model_executor.model_loader.loader import (BaseModelLoader,
                                                      get_model_loader)
 from vllm.model_executor.model_loader.utils import (
